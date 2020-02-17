68d5614abb068d43573e56ca99bd4136d12a2c83
PilarPinto <piapintoch@unal.edu.co>
2020-02-17 23:15:25 +0000

    [Feature]-Creates an Init for FileStorage

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-Create a unique FileStorage instance
+Create a unique FileStorage instance, import of
+the class Storage and creates an  intance to call
+the reload function
 """
 from models.engine.file_storage import FileStorage
 
