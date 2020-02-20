cb19171be3c63e5af0a4039327242342dfac2c8e
juan-skill <1294@holbertonschool.com>
2020-02-20 09:37:38 +0000

    [Fix] changing the Setup method 	modified:   tests/test_models/test_engine/test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-
+"""
+Test for FileStorage Class
+"""
 import unittest
 import pep8
+import os
 
 from models.base_model import BaseModel
-from models.engine.filestorage import FileStorage
+from models.user import User
+from models.engine.file_storage import FileStorage
 
 from datetime import datetime
 
@@ -14,22 +18,22 @@ class TestStringMethods(unittest.TestCase):
     Testing class
     """
 
-    def setUp(self):
-        self.store = FileStorage()
+    @classmethod
+    def setUpClass(cls):
+        """set up for test"""
+        cls.user = User()
+        cls.user.first_name = "Adrian"
+        cls.user.last_name = "Hernandez"
+        cls.user.email = "1214@holbertonschool.com"
+        cls.storage = FileStorage()
 
-        test_args = {'updated_at': datetime(2019, 2, 20, 00, 01, 43, 331997),
-                     'id': 'f519fb40-1f5c-458b-945c-2ee8eaaf4900',
-                     'created_at': datetime(2017, 2, 12, 00, 31, 53, 331900)}
-        self.model = BaseModel(test_args)
-
-        self.test_len = 0
-        if os.path.isfile("file.json"):
-            self.test_len = len(self.store.all())
-
-    def tearDown(self):
-        import os
-        if os.path.isfile("file.json"):
-            os.remove('file.json')
+    def test_all(self):
+        """ Tests if all works in File Storage"""
+        storage = FileStorage()
+        obj = storage.all()
+        self.assertIsNotNone(obj)
+        self.assertEqual(type(obj), dict)
+        self.assertIs(obj, storage._FileStorage__objects)
 
     def test_pep8_conformance(self):
         """Test that we conform to PEP8."""
@@ -37,6 +41,11 @@ class TestStringMethods(unittest.TestCase):
         result = pep8style.check_files(['models/engine/file_storage.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
+
+    @classmethod
+    def teardown(cls):
+        """at the end of the test this will tear it down"""
+        del cls.user
 
 if __name__ == '__main__':
     unittest.main()
